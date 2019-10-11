0d77c22f1ec5a19d1059743878920f34dc4669bb
geraldgsh <geraldgsh@hotmail.com>
2019-10-12 05:16:56 +0800

    Added block error message

--- main.rb
+++ main.rb
@@ -16,11 +16,13 @@ def bubble_sort_by(arr)
         arr[j], arr[j - 1] = arr[j - 1], arr[j] if yield(arr[j - 1], arr[j]).positive?
       elsif arr[j - 1] > arr[j]
         arr[j], arr[j - 1] = arr[j - 1], arr[j]
+      else
+        'No block given!'
       end
     end
   end
   arr
 end
 
-# puts bubble_sort([3,2,1])
+puts bubble_sort([3, 2, 1])
 puts bubble_sort_by(%w[hi hello hey]) { |left, right| left.length - right.length }
